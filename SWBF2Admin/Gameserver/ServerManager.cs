6f342d03bea9d197fbbfabfc32746626cb00313a
Jan Weigelt <info@janelo.net>
2024-03-19 14:41:47 +0100

    Remove dll injector for aspyr build

--- SWBF2Admin/Gameserver/ServerManager.cs
+++ SWBF2Admin/Gameserver/ServerManager.cs
@@ -269,7 +269,7 @@ namespace SWBF2Admin.Gameserver
 
         private void InjectRconDllIfRequired()
         {
-            if (serverType == GameserverType.GoG || serverType == GameserverType.Steam || serverType == GameserverType.Aspyr)
+            if (serverType == GameserverType.GoG || serverType == GameserverType.Steam)
             {
                 string loader;
                 string dll;
